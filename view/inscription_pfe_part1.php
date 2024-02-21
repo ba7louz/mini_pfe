404fc5691ce9f3c6684220f2934d182b4632b8e8
rami <ramisassi11@gmail.com>
2024-02-21 19:08:32 +0100

    aaa

--- view/inscription_pfe_part1.php
+++ view/inscription_pfe_part1.php
@@ -1,4 +1,51 @@
-<div>
+<style>
+    body {
+        background: linear-gradient(to bottom, #ffffff, #87ceeb, #3498db);
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+    .form-container {
+        width: 800px; 
+        margin: 0 auto;
+        border: 5px solid #ccc; 
+        padding: 20px; 
+        border-radius: 10px; 
+        background-color: white; /* Ajout de cette ligne */
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    form {
+        width: 100%;
+    }
+
+    label,
+    input,
+    select,
+    textarea {
+        display: block;
+        width: 100%;
+        margin-bottom: 10px;
+    }
+
+    textarea {
+        height: 100px;
+    }
+
+    input[type=submit] {
+        background: linear-gradient(to bottom, #ffffff, #87ceeb, #3498db);
+        color: white;
+        border: none;
+        padding: 10px;
+        cursor: pointer;
+    }
+</style>
+
+
+<div class="form-container">
     <h1>Inscription pour PFE (années)</h1>
     <form action="../controller/inscription_pfe_part1.php" method="post">
         <h2>Etudiant 1</h2>
@@ -19,30 +66,44 @@
         <label for="">
             Titre Projet* <input type="text" name="titre" placeholder="Saisir votre titre de projet"  >
         </label> <br>
-        <input type="submit" value="Suivant" >
+        <label for="">
+            Encadreur_ISET <input type="text" name="encadreur_iset" placeholder="Saisir votre encadreur de projet">
+        </label><br>
+        <label for="">
+            Nom_entreprise* <input type="text" name="nom_entreprise" placeholder="Saisir le nom de l'entreprise" required>
+        </label><br>
+        <label for="">
+            Encadreur_entreprise <input type="text" name="encadreur_entreprise" placeholder="saisir le nom de l'encadreur de l'entreprise">
+        </label><br>
+        <label for="">Importer fiche PFE:
+            <input type="file" name="fiche_pfe" accept=".pdf">
+        </label><br>
+        <input type="submit" value="Suivant">
     </form>
 </div>
 
 <script>
-    document.getElementById('cin_ip').addEventListener('keyup',()=>{
+    document.getElementById('cin_ip').addEventListener('keyup', () => {
         let str = document.getElementById('cin_ip').value;
         cin_existence(str);
     })
+
     function cin_existence(str) {
-    if (str.length === 0) {
-        document.getElementById("fullname2").value = "" ;
-        return;
+        if (str.length === 0) {
+            document.getElementById("fullname2").value = "";
+            return;
+        }
+        fetch("getcinAjax.php?getNameByCin=" + str)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("fullname2").value = data;
+            })
     }
-    fetch("getcinAjax.php?getNameByCin=" + str)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("fullname2").value = data;
-        })
-    }
+
     let errorMessageElement = document.getElementById('err');
     if (errorMessageElement) {
         setTimeout(() => {
             errorMessageElement.remove();
-        }, 15000); // Remove the element after 15 seconds
+        }, 15000); 
     }
 </script>
