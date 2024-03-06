be7c48a2b3eecdb1548a715b668a09517b3c856a
rami <ramisassi11@gmail.com>
2024-03-06 08:49:15 +0100

    gg

--- controller/binomes.php
+++ controller/binomes.php
@@ -94,6 +94,12 @@ if(isset($_GET['pfe_id'])) {
                 echo "<script>alert('sent')</script>";
                 header ("location:admin.php");
             }elseif($action == 'refuser'){
+                $sql_update_validite_date = "UPDATE pfe SET validite = 1, date_reponse = :date_reponse WHERE id = :pfe_id";
+                $stmt_update_validite_date = $connexion->prepare($sql_update_validite_date);
+                $current_date = date("Y-m-d H:i:s"); 
+                $stmt_update_validite_date->bindParam(':date_reponse', $current_date);
+                $stmt_update_validite_date->bindParam(':pfe_id', $pfe_id);
+                $stmt_update_validite_date->execute();  
                 $mail = new PHPMailer(true);
                 $mail->isSMTP();
                 $mail->Host = "smtp.gmail.com";
@@ -109,6 +115,7 @@ if(isset($_GET['pfe_id'])) {
                 $mail->Body = "Votre demande d'inscrit a été refusé .";
                 $mail->send();
                 echo "<script>alert('sent')</script>";
+                header ("location:admin.php");
             }
         }
 
